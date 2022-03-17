b9254cfd4f9577600fb77d17be3fd674a7f2340b
felicanx <96733551+felicanx@users.noreply.github.com>
2022-03-17 15:00:54 +0530

    Update fav.py

--- fav.py
+++ fav.py
@@ -5,7 +5,7 @@ import mmh3
 import requests
 import codecs
 
-response = requests.get('https://www.usbr.gov/img/favicon.ico')
+response = requests.get('enter-domain-here')
 favicon = codecs.encode(response.content, "base64")
 hash = mmh3.hash(favicon)
 print(hash)
